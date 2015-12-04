6d83d5d16a4dfc54ef2fbba76745774fa86c3fcd
Cody Russell <cody@jhu.edu>
2015-12-04 00:03:36 -0600

    Make users invitable.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
