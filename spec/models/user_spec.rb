df05f90b91fdec6ea7f6fae3bd695a7ef4ab9fa5
Cody Russell <cody@jhu.edu>
2015-12-04 00:09:49 -0600

    Add a spec for user auth tokens.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when creating a new user' do
+    subject(:user) {
+      User.create!(
+        email: 'batman@thebatcave.com',
+        password: 'aquamansuxsomuch',
+        password_confirmation: 'aquamansuxsomuch'
+      )
+    }
+
+    it 'should have an auth token' do
+      expect(user.auth_token).to_not be_blank
+    end
+  end
 end
