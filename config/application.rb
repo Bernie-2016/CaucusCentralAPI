1c356dbaf2e571a933be6f90790959bc8fee89d1
Cody Russell <cody@jhu.edu>
2015-12-03 22:49:03 -0600

    Generators config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module CaucusCentralAPI
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework      :rspec, fixture: true
+      g.fixture_replacement :fabrication
+      g.assets = false
+      g.helper = false
+    end
   end
 end
