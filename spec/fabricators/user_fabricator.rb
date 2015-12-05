f4d6988ed9029047a711bc319c53e03261fb8c0b
Cody Russell <cody@jhu.edu>
2015-12-05 14:55:15 -0600

    User fabricator.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,2 +1,14 @@
 Fabricator(:user) do
+  first_name 'Bruce'
+  last_name  'Wayne'
+  email      { sequence(:email) { |i| "bruce#{i}@thebatcave.com" } }
+  password   'aquamansuxsomuch'
+  auth_token { sequence(:auth_token) { |i| "aquamansuxevenmorethanyouknow#{i}" } }
+end
+
+Fabricator(:confirmed_user, from: :user) do
+  first_name 'Clark'
+  last_name  'Kent'
+  email      { sequence(:email) { |i| "clark#{i}@thedailyplanet.com" } }
+  confirmation_token Time.now
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'when creating a new user' do
-    subject(:user) {
-      User.create!(
-        email: 'batman@thebatcave.com',
-        password: 'aquamansuxsomuch',
-        password_confirmation: 'aquamansuxsomuch'
-      )
-    }
+    subject(:user) {Fabricate(:confirmed_user) }
 
     it 'should have an auth token' do
       expect(user.auth_token).to_not be_blank
