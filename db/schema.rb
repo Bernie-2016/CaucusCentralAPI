89a6c6be7c8264313c1fe0c3071fda9bc33901b4
Zach Schneider <zach@schneid.io>
2016-01-21 20:41:50 -0600

    Add timestamps to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120033956) do
+ActiveRecord::Schema.define(version: 20160122024112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,14 +57,16 @@ ActiveRecord::Schema.define(version: 20160120033956) do
   end
 
   create_table "users", id: :bigserial, force: :cascade do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.string  "email"
-    t.string  "password_digest"
-    t.integer "privilege"
-    t.integer "precinct_id"
-    t.integer "invitation_id"
-    t.string  "phone_number"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "privilege"
+    t.integer  "precinct_id"
+    t.integer  "invitation_id"
+    t.string   "phone_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["precinct_id"], name: "index_users_on_precinct_id", using: :btree
