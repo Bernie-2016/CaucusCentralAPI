7a79a242ba234c05be63dfb315573232be07eaf8
Zach Schneider <zach@schneid.io>
2016-01-24 14:18:13 -0600

    Fix simplecov groups

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,9 +2,16 @@ require 'codeclimate-test-reporter'
 require 'simplecov'
 
 SimpleCov.formatter = CodeClimate::TestReporter::Formatter if ENV['CIRCLE_ARTIFACTS']
-SimpleCov.start 'rails' do
+SimpleCov.start do
   add_filter 'app/workers'
+  add_filter 'config'
+  add_filter 'db'
+  add_filter 'spec'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Mailers', 'app/mailers'
   add_group 'Serializers', 'app/serializers'
+  add_group 'Services', 'app/services'
   coverage_dir 'spec/coverage'
 end
 
