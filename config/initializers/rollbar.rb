b426343c0485247ce0cab83c93ccc48f1ef0c9ec
Zach Schneider <zach@schneid.io>
2016-02-01 14:50:48 -0600

    Enable rollbar in prod

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,6 @@
 if Rails.env.prduction?
   Rollbar.configure do |config|
+    config.enabled = true
     config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
     config.environment = ENV['ROLLBAR_ENV'] || Rails.env
   end
