1df3c28085ea20726aa34c088aec3436b33c0177
Zach Schneider <zach@schneid.io>
2016-02-25 12:02:19 -0600

    Include invite token in API response

--- app/serializers/invitation_serializer.rb
+++ app/serializers/invitation_serializer.rb
@@ -1,7 +1,7 @@
 class InvitationSerializer < JsonSerializer
   class << self
     def hash(invitation, _options = {})
-      node = hash_for(invitation, %w(id email privilege precinct_id))
+      node = hash_for(invitation, %w(id email privilege precinct_id token))
       node[:precinct_name] = invitation.precinct.try(:name)
       node[:precinct_state] = invitation.precinct.try(:state).try(:code)
       node
